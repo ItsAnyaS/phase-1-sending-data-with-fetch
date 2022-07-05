aaecf5c1dd979e5fe35632c02d7548d06ffb9f4f
Anastasia <derricksmac@Anyas-MacBook-Air.local>
2022-07-05 15:34:56 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+const submitData = async (nameInput, emailInput) => {
+
+
+try {
+let req = await fetch('http://localhost:3000/users',{
+method: 'POST',
+headers: {'Content-type': 'application/json', 'Accept': 'application/json'},
+body:JSON.stringify({name: nameInput, email: emailInput})
+})
+let res = await req.json()
+return res
+console.log(res.id)
+}
+catch{
+let err = document.createElement('p')
+err.innerText = res
+document.body.append(err)
+}
+
+
+}
+
+submitData('hi', 'there')
