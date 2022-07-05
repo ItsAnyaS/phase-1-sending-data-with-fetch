0adfaa0abe74543106fc9fae8b80b370cf199506
Anastasia <derricksmac@Anyas-MacBook-Air.local>
2022-07-05 15:22:18 -0400

    commit tree fd8fc910757d3fae654718d3a8bcb4546c3b10a2

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Add your code here
+const submitData = async (name, email) => {
+let req = await fetch('http://localhost:3000/users',{
+method: 'POST',
+headers: {'Content-type': 'application.json'},
+body:JSON.stringify(name, email)
+})
+let res = await req.json
+
+console.log(res)
+}
+
+submitData('hi', 'there')
